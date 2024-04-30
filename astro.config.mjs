6698229d988fb86321b20a81a6dddd1dc9d17a1b
Iván Ezequiel Iencinella <iiencinella@outlook.es>
2024-04-30 01:47:06 -0300

    Add vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
+import vercelServerless from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  }),
+  // adapter: node({
+  //   mode: "standalone"
+  // }),
+  adapter: vercelServerless(),
   integrations: [tailwind()]
 });
