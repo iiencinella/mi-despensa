161b4bcdc82149ee5dd388892b8f28ae82a60f63
Iván Ezequiel Iencinella <iiencinella@outlook.es>
2024-05-02 08:31:06 -0300

    Añado login anterior

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -14,6 +14,22 @@ export function dbLogin(email: string, password: string) {
         role = user[0].role
       }
     }
+    else {
+      if (email === "admin@example.com" && password === "1234") {
+        username = "Usuario Admin";
+        role = 'admin';
+      } else if (email === "proveedor@example.com" && password === "1234") {
+        username = "Ususario Proveedor";
+        role = 'proveedor';
+      } else if (email === "producto@example.com" && password === "1234") {
+        username = "Usuario Producto";
+        role = 'producto';
+      } else if (email === "cajas@example.com" && password === "1234") {
+        username = "Usuario Cajas";
+      } else {
+        success = false;
+      }
+    }
     resolve({success, username, role});
   });
 }
